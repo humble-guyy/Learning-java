96889fc53c0e369f0aa8a89b0402bf7288234426
Pranjal Agarwal <agarwalpranjal2006@gmail.com>
2024-01-27 11:02:26 +0530

    datatypes and variables

---
+++ day1/datatypesandvaraibles.java
@@ -0,0 +1,32 @@
+package day1;
+import java.util.*;
+public class datatypesandvaraibles {
+    public static void main(String[] args){
+    //this is a single line comment
+    /*this is a 
+     * multi line comment
+     */
+    int num1=153;
+    int num2=306;
+    System.out.print("The division of NUM1 and NUM2  is:");
+    System.out.println(num1/num2);
+    boolean b1=true;
+    System.out.println(b1);
+    char ch='a';
+    System.out.println(ch);
+    byte by=123;
+    int intdata=1234;
+    System.out.println(by);
+    System.out.println(intdata);
+    double dl=123456.12;
+    float f1=123456.12f;
+    System.out.println(f1);
+    System.out.println(dl);
+    //not primitive datatypes
+    String pranjal="Pranjal Agarwal";
+    System.out.println(pranjal);
+    //array
+    int result[]={50,60,70};
+    System.out.print(result[0]);
+    }
+}
