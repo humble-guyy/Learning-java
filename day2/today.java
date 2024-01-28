bf4cf64eb8c29dff588ef1c12d6cd00daace37db
Pranjal Agarwal <agarwalpranjal2006@gmail.com>
2024-01-28 23:28:13 +0530

    operators

---
+++ day2/today.java
@@ -0,0 +1,19 @@
+package day2;
+import java.util.*;
+public class today {
+    public static void main(String[] args){
+    int num1=5;
+    int num2=6;
+    int result=num1+num2;
+    System.out.println(result);
+    boolean res=3!=2;
+    int num3=7;
+    System.out.println(res);
+    System.out.println(2>2 && 2!=3);
+    System.out.println(2>2 & 2!=3);
+    System.out.println(num1<num2 && num2++>num3);
+    System.out.println(num1<num2 & num2++>num3);
+
+
+    }
+}
